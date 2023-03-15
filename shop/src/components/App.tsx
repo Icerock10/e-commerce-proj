7d5c1c7e2bb01e831688040610acfae8fd8bd7e1
Icerock10 <dp021190dms@gmail.com>
2023-03-15 15:26:47 +0200

    first commit

---
+++ shop/src/components/App.tsx
@@ -0,0 +1,17 @@
+import '../styles/style.scss';
+import Carousel from './Carousel';
+
+const users: number[] = [1,4,6,10,6,7];
+
+
+function App() {
+  return (
+	<div className='wrapper'>
+    <Carousel/>
+	 </div>
+  )
+}
+
+export default App
+
+
