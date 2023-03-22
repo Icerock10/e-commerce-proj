e8f6e5d58d8e72bb7d6b121cefb1894e9bedac54
Icerock10 <dp021190dms@gmail.com>
2023-03-22 12:44:35 +0200

    added titles as array

--- shop/src/helpers/Frames.tsx
+++ shop/src/helpers/Frames.tsx
@@ -4,35 +4,30 @@ import Frame3 from "../assets/images/Frame3.png";
 import Frame5 from "../assets/images/Frame5.png";
 import Frame6 from "../assets/images/Frame6.png";
 
-export const images = [
+interface Frame {
+  id: number;
+  image: string;
+}
+
+export const frames: Frame[] = [
   {
     id: 1,
     image: `${banner}`,
-    title: "Get start",
-    subTitle: "Your favorite shopping",
   },
   {
     id: 2,
     image: `${Frame1}`,
-    title: "There's more",
-    subTitle: "here than you expect",
   },
   {
     id: 3,
     image: `${Frame3}`,
-    title: "We are not looking",
-    subTitle: "for a reason to make gifts",
   },
   {
     id: 5,
     image: `${Frame5}`,
-    title: "We are the ones who",
-    subTitle: "certainly justify the trust!",
   },
   {
     id: 6,
     image: `${Frame6}`,
-    title: "Ready to",
-    subTitle: "provide everything you need!",
   },
 ];
