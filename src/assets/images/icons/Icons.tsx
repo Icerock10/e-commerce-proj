c43b9a754ebc6225bffaee65283b944ae1929f6c
Icerock10 <dp021190dms@gmail.com>
2023-04-21 16:46:06 +0300

    added adaptive styles

--- src/assets/images/icons/Icons.tsx
+++ src/assets/images/icons/Icons.tsx
@@ -319,8 +319,8 @@ const ChevronUp = () => {
 const CheckMarkIcon = () => {
   return (
     <svg
-      width="50"
-      height="50"
+      width="45"
+      height="45"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
